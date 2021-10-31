0be38aed6d73937e6c6145950da9dd095b26de52
Johnny <johnnylabedzki@hotmail.com>
2021-10-31 15:49:49 -0700

    Change db url in .env

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -3,13 +3,7 @@ import { Pool } from "pg";
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  // FOR USE WITH HEROKU
-  // max: 6,
-  // connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || "",
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 export default pool;
