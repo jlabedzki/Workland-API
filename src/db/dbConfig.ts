6b5030a94865b362679a924d317b7852128df170
Johnny <johnnylabedzki@hotmail.com>
2021-10-31 15:55:36 -0700

    change dbconfig

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -3,7 +3,13 @@ import { Pool } from "pg";
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "",
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // user: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  // FOR USE WITH HEROKU
+  max: 6,
+  connectionString: process.env.DATABASE_URL,
 });
 export default pool;
