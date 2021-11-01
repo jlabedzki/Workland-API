a22c835a673e05b3d16ccc53101332d4edf3c430
Johnny <johnnylabedzki@hotmail.com>
2021-11-01 11:03:16 -0700

    app.set trust proxy

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@ import { socketServer } from "./socketServer";
 const cors = require("cors");
 const app: Application = express();
 const port = process.env.PORT || 5000;
+app.set("trust proxy", 1); // trust first proxy
 
 app.use(cors());
 app.use(function (req, res, next) {
